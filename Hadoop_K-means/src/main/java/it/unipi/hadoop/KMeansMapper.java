e609d11dfe0a32d8de23c5b713d14ff1bd130b5d
Federico Montini <74728075+FedericoMontini98@users.noreply.github.com>
2023-06-14 15:49:07 +0200

    Update KMeansMapper.java

--- Hadoop_K-means/src/main/java/it/unipi/hadoop/KMeansMapper.java
+++ Hadoop_K-means/src/main/java/it/unipi/hadoop/KMeansMapper.java
@@ -1,23 +1,19 @@
 package main.java.it.unipi.hadoop;
 
 
-import org.apache.hadoop.fs.FileSystem;
-import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Mapper;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
-import java.util.Arrays;
 
 public class KMeansMapper extends Mapper<Object, Text, Centroid, Point> {
 	
-	private final static ArrayList<Centroid> centroids = new ArrayList<Centroid>(); // Centroid List
-	private static int k; // Centroid number
+	private static ArrayList<Centroid> centroids;
 	
 	/**
 	 * @param key
@@ -54,57 +50,40 @@ public class KMeansMapper extends Mapper<Object, Text, Centroid, Point> {
 	/**
 	 * @param context Information about centroid-point association
 	 * @throws IOException Thrown when an I/O error occurs during Cached file reading
-	 * @throws InterruptedException thrown when a thread is interrupted while it's waiting, sleeping, or otherwise occupied
 	 */
 	@Override
 	protected void setup(Context context) throws IOException, InterruptedException {
+
 		// Read centroids returned from previous iteration inside the distributed cache
-		try {
-			URI[] cacheFiles = context.getCacheFiles();
-			if (cacheFiles != null && cacheFiles.length > 0) {
-				// Read centroids from a file inside the distributed cache
-				Path centroidPath = new Path(cacheFiles[0].toString());
-				FileSystem fs = FileSystem.get(context.getConfiguration());
-				try (BufferedReader br = new BufferedReader(new InputStreamReader(fs.open(centroidPath)))) {
-					String line;
-					while ((line = br.readLine()) != null) {
-						// TODO: Parsa e aggiungi i centroidi alla lista
-					}
-				}
-			}
-		} catch (IOException e) {
-			System.err.println("Error reading centroids from cache: " + e.getMessage());
+		URI[] cacheFiles = context.getCacheFiles();
+		if (cacheFiles != null && cacheFiles.length > 0) {
+			// Read the centroids from the cached file
+			centroids = readCentroids(cacheFiles[0].getPath());
+			return;
 		}
-		
-		// If we are in the first iteration no centroids will be found, initialize them randomly
-		if (centroids.isEmpty()) {
-			generate_random_centroids(context);
-		}
+		// If we are in the first iteration no centroids will be found, get random ones
+		super.setup(context);
+		String inputPath = context.getConfiguration().get("centroids");
+		centroids = readCentroids(inputPath);
 	}
 	
-	private void generate_random_centroids(Context context){
-		// TODO SET IN CONFIGURATION
-		int num_coordinates = context.getConfiguration().getInt("num_coordinates", 10);
-		int MAX_VALUE = context.getConfiguration().getInt("max_value", Integer.MAX_VALUE);
-		
-		// Choose k random indices without replacement
-		for (int i = 0; i < k; i++) {
-			//Generate randomly new coordinates
-			ArrayList<Double> coordinates = new ArrayList<>();
-			// Generate random value for each coordinate
-			for(int n = 0; n < num_coordinates; n++ ) {
-				coordinates.add(Math.random() * MAX_VALUE);
-			}
-			
-			Point point = new Point(coordinates);
-			//Add new element to ArrayList
-			centroids.add(new Centroid());
-			// Set new Point
-			centroids.get(i).setPoint(point);
-			// Set new centroid ID
-			IntWritable new_id = new IntWritable(i);
-			centroids.get(i).setCentroid_id(new_id);
+	/** Extract the centroids from file
+	 * @param path path to the file that contains the centroids
+	 * @return ArrayList of Centroid objects
+	 * @throws IOException if an I/O error occurs during file reading
+	 */
+	private ArrayList<Centroid> readCentroids(String path) throws IOException {
+		ArrayList<Centroid> centroids = new ArrayList<>();
+		DataInputStream in = new DataInputStream(Files.newInputStream(Paths.get(path)));
+		while (in.available() > 0) {
+			// Create a new Centroid object
+			Centroid centroid = new Centroid();
+			// Deserialize the centroid from the input stream
+			centroid.readFields(in);
+			centroids.add(centroid);
 		}
+		in.close();
+		return centroids;
 	}
 }
 
