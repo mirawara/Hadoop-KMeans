a56f926ebc37b501d519dab06930c9edf1de0ef9
Federico Montini <74728075+FedericoMontini98@users.noreply.github.com>
2023-06-19 18:35:59 +0200

    Added Exception handlers and removed renaming of part-r-00000 file

--- Hadoop_K-means/src/main/java/it/unipi/hadoop/KMeans.java
+++ Hadoop_K-means/src/main/java/it/unipi/hadoop/KMeans.java
@@ -8,83 +8,113 @@ import org.apache.hadoop.mapreduce.Job;
 import java.io.IOException;
 import java.util.ArrayList;
 
-//TODO: gestione delle eccezioni
+//TODO: gestione delle eccezioni - Manca solo quella causata da Mergeoutput che deve essere tolta
 public class KMeans {
-
-    private static void KMeansIterations(Configuration conf, Path outputPath, Path inputPath, int numReducers) throws IOException {
-
-        FileSystem fs = FileSystem.get(conf);
-
-        boolean converged = false;
-        int iteration = 0;
-
-        while (!converged && iteration < KMeansUtil.DEFAULT_MAX_ITERATIONS) {
-
-            fs.delete(outputPath, true);
-
-            try (Job job = KMeansUtil.configureJob(conf, inputPath, outputPath, numReducers, iteration)) {
-                if (job == null) {
-                    System.err.println("Error in Job configuration");
-                    System.exit(1);
-                }
-                if (!job.waitForCompletion(true)) {
-                    System.err.println("Error during Job execution");
-                    System.exit(1);
-                }
-
-            } catch (IOException | InterruptedException | ClassNotFoundException e) {
-                e.printStackTrace();
-            }
-
-
-            //TODO: non conviene fare il merge ma utilizzare un metodo per leggere i file dei reducer (più efficiente e anche più semplice)
-            if (numReducers > 1) {
-                KMeansUtil.mergeOutput(conf, outputPath, iteration);
-            } else {
-                //TODO: togliere il renaming perché abbiamo già il log, è solo una perdita di tempo
-                fs.rename(new Path(outputPath + "/part-r-00000"), new Path(outputPath + KMeansUtil.OUTPUT_NAME + iteration));
-            }
-
-            // Read current centroids
-            String currentCentroidFile = outputPath + KMeansUtil.OUTPUT_NAME + iteration;
-            ArrayList<Centroid> currentCentroids = KMeansUtil.readCentroids(currentCentroidFile, conf, false);
-            double shift = KMeansUtil.calculateCentroidShift(currentCentroids, conf);
-            converged = (shift < KMeansUtil.DEFAULT_THRESHOLD);
-
-            try {
-                KMeansUtil.logIterationInfo(iteration, shift);
-            } catch (IOException e) {
-                System.err.println("Error during the write on the MapReduce log file");
-                e.printStackTrace();
-            }
-
-            if (!converged) {
-                KMeansUtil.setCentroidsToConf("centroids", currentCentroids, conf);
-            }
-            iteration++;
-        }
-    }
-
-    public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
-        if (args.length < 3 || args.length > 4) {
-            System.err.println("Usage: KMeansMain <inputPath> <outputPath> <centroidPath> [<numReducers>]");
-            System.exit(1);
-        }
-
-        Path inputPath = new Path(args[0]);
-        Path outputPath = new Path(args[1]);
-        String centroidPath = args[2];
-
-        int numReducers = (args.length == 4) ? Integer.parseInt(args[3]) : KMeansUtil.DEFAULT_NUM_REDUCERS;
-
-
-        Configuration conf = new Configuration();
-
-        // Set centroids in the configuration
-
-        KMeansUtil.setCentroidsToConf("centroids", KMeansUtil.readCentroids(centroidPath, conf, true), conf);
-
-        KMeansIterations(conf, outputPath, inputPath, numReducers);
-
-    }
+	
+	private static void KMeansIterations(Configuration conf, Path outputPath, Path inputPath, int numReducers) throws IOException {
+		
+		// Get configuration file
+		FileSystem fs = null;
+		try {
+			fs = FileSystem.get(conf);
+		} catch (IOException e) {
+			System.err.println("Error during configuration file reading: " + e.getMessage());
+			System.exit(1);
+		}
+		
+		boolean converged = false;
+		int iteration = 0;
+		
+		while (!converged && iteration < KMeansUtil.DEFAULT_MAX_ITERATIONS) {
+			
+			try {
+				fs.delete(outputPath, true);
+			} catch (IOException e) {
+				System.err.println("Error during the deletion of the output file: " + e.getMessage());
+				System.exit(1);
+			}
+			
+			try (Job job = KMeansUtil.configureJob(conf, inputPath, outputPath, numReducers, iteration)) {
+				if (job == null) {
+					System.err.println("Error in Job configuration");
+					System.exit(1);
+				}
+				if (!job.waitForCompletion(true)) {
+					System.err.println("Error during Job execution");
+					System.exit(1);
+				}
+				
+			} catch (IOException | InterruptedException | ClassNotFoundException e) {
+				e.printStackTrace();
+			}
+			
+			
+			//TODO: non conviene fare il merge ma utilizzare un metodo per leggere i file dei reducer (più efficiente e anche più semplice)
+			if (numReducers > 1) {
+				KMeansUtil.mergeOutput(conf, outputPath, iteration);
+			}
+			
+			// Read current centroids
+			String currentCentroidFile = outputPath + "/part-r-00000";
+			
+			ArrayList<Centroid> currentCentroids = null;
+			try {
+				currentCentroids = KMeansUtil.readCentroids(currentCentroidFile, conf, false);
+			} catch (IOException e) {
+				System.err.println("Error during the reading of the current centroids: " + e.getMessage());
+				System.exit(1);
+			}
+			double shift = KMeansUtil.calculateCentroidShift(currentCentroids, conf);
+			
+			converged = (shift < KMeansUtil.DEFAULT_THRESHOLD);
+			if (!converged) {
+				KMeansUtil.setCentroidsToConf("centroids", currentCentroids, conf);
+			}
+			
+			try {
+				KMeansUtil.logIterationInfo(iteration, shift);
+			} catch (IOException e) {
+				System.err.println("Error during the write on the MapReduce log file: " + e.getMessage());
+				e.printStackTrace();
+			}
+			
+			iteration++;
+		}
+	}
+	
+	public static void main(String[] args) throws IOException {
+		if (args.length < 3 || args.length > 4) {
+			System.err.println("Usage: KMeansMain <inputPath> <outputPath> <centroidPath> [<numReducers>]");
+			System.exit(1);
+		}
+		
+		Path inputPath = null;
+		Path outputPath = null;
+		String centroidPath = null;
+		int numReducers = 1;
+		
+		try {
+			inputPath = new Path(args[0]);
+			outputPath = new Path(args[1]);
+			centroidPath = args[2];
+			numReducers = (args.length == 4) ? Integer.parseInt(args[3]) : KMeansUtil.DEFAULT_NUM_REDUCERS;
+		} catch (IllegalArgumentException e) {
+			System.err.println("Error during the parsing of the arguments: " + e.getMessage());
+			System.exit(1);
+		}
+		
+		
+		Configuration conf = new Configuration();
+		
+		// Set centroids in the configuration
+		try {
+			KMeansUtil.setCentroidsToConf("centroids", KMeansUtil.readCentroids(centroidPath, conf, true), conf);
+		} catch (IOException e) {
+			System.err.println("Error during the reading of the centroids: " + e.getMessage());
+			System.exit(1);
+		}
+		
+		KMeansIterations(conf, outputPath, inputPath, numReducers);
+		
+	}
 }
--- Hadoop_K-means/src/main/java/it/unipi/hadoop/KMeansUtil.java
+++ Hadoop_K-means/src/main/java/it/unipi/hadoop/KMeansUtil.java
@@ -19,142 +19,142 @@ import java.util.Collections;
 import java.util.stream.Collectors;
 
 public class KMeansUtil {
-
-    public static final int DEFAULT_NUM_REDUCERS = 1;
-    public static final double DEFAULT_THRESHOLD = 0.001;
-    public static final int DEFAULT_MAX_ITERATIONS = 20;
-
-    public static final String OUTPUT_NAME = "/output_centroids";
-
-
-    /**
-     * Extract the centroids from file
-     *
-     * @param pathString path to the file that contains the centroids
-     * @return ArrayList of Centroid objects
-     * @throws IOException if an I/O error occurs during file reading
-     */
-    public static ArrayList<Centroid> readCentroids(String pathString, Configuration conf, boolean csvReading) throws IOException {
-        ArrayList<Centroid> centroids = new ArrayList<>();
-        Path path = new Path(pathString);
-        FileSystem hdfs = FileSystem.get(conf);
-        FSDataInputStream in = hdfs.open(path);
-        BufferedReader reader = new BufferedReader(new InputStreamReader(in));
-        String line;
-
-        while ((line = reader.readLine()) != null) {
-
-            String[] fields = line.split("\\s");
-
-            if (csvReading) {
-                fields = line.split(",");
-            }
-
-            // Creare un nuovo oggetto Centroid
-            Centroid centroid = new Centroid();
-
-            // Impostare l'ID del centroide
-            int centroidId = (int) Double.parseDouble(fields[0]);
-            centroid.getCentroid_id().set(centroidId);
-
-            ArrayList<Double> coords = new ArrayList<>();
-            for (int i = 1; i < fields.length; i++) {
-                double value = Double.parseDouble(fields[i]);
-                coords.add(value);
-            }
-            Point point = new Point(coords);
-            centroid.setPoint(point);
-            centroids.add(centroidId, centroid);
-        }
-
-        in.close();
-        Collections.sort(centroids);
-        return centroids;
-    }
-
-    public static Job configureJob(Configuration conf, Path inputPath, Path outputPath, int numReducers, int iteration) {
-        Job job;
-        try {
-            job = Job.getInstance(conf, "K-Means Iteration " + iteration);
-            job.setJarByClass(KMeans.class);
-            job.setMapperClass(KMeansMapper.class);
-            job.setCombinerClass(KMeansCombiner.class);
-            job.setMapOutputKeyClass(IntWritable.class);
-            job.setMapOutputValueClass(Point.class);
-            job.setNumReduceTasks(numReducers);
-            job.setReducerClass(KMeansReducer.class);
-            job.setOutputKeyClass(IntWritable.class);
-            job.setOutputValueClass(Text.class);
-            FileInputFormat.addInputPath(job, inputPath);
-            FileOutputFormat.setOutputPath(job, outputPath);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            return null;
-        }
-
-        return job;
-    }
-
-    public static void mergeOutput(Configuration conf, Path outputPath, int iteration) throws IOException {
-        FileSystem fs = FileSystem.get(conf);
-
-        Path outputFile = new Path(outputPath, OUTPUT_NAME + iteration);
-        OutputStream out = fs.create(outputFile);
-        for (FileStatus fileStatus : fs.listStatus(outputPath)) {
-            if (fileStatus.isFile() && !fileStatus.getPath().equals(outputFile)) {
-                InputStream in = fs.open(fileStatus.getPath());
-                IOUtils.copyBytes(in, out, conf);
-                in.close();
-            }
-        }
-        out.close();
-    }
-
-    public static ArrayList<Centroid> readCentroidsFromConfiguration(Configuration conf) {
-
-        ArrayList<Centroid> centroids = new ArrayList<>();
-
-        String[] centroidStrings = conf.getStrings("centroids");
-        // Get the values of the centroids
-        for (int i = 0; i < centroidStrings.length; i++) {
-            centroids.add(new Centroid(i, Arrays.stream(centroidStrings[i].split(" "))
-                    .map(Double::parseDouble)
-                    .collect(Collectors.toCollection(ArrayList::new))));
-        }
-        return centroids;
-    }
-
-    public static double calculateCentroidShift(ArrayList<Centroid> currentCentroids, Configuration conf) {
-
-        // Read previous centroids
-        ArrayList<Centroid> previousCentroids = readCentroidsFromConfiguration(conf);
-
-        // Calculate centroid shift
-        double shift = 0.0;
-        for (int i = 0; i < previousCentroids.size(); i++) {
-            ArrayList<Double> previousCentroidCoord = previousCentroids.get(i).getPoint().getCoordinates();
-            ArrayList<Double> currentCentroidCoord = currentCentroids.get(i).getPoint().getCoordinates();
-            for (int j = 0; j < previousCentroidCoord.size(); j++) {
-                shift += Math.abs(currentCentroidCoord.get(j) - previousCentroidCoord.get(j));
-            }
-        }
-        return shift;
-    }
-
-    public static void setCentroidsToConf(String key, ArrayList<Centroid> centroids, Configuration conf){
-        //[centroid.getPoint().toString() for centroid in centroids]
-        //Per ogni centroide nella lista di centroidi mi prendo la stringa delle coordinate del punto
-        conf.setStrings(key, centroids.stream()
-                .map(centroid -> centroid.getPoint().toString())
-                .toArray(String[]::new));
-    }
-
-    public static void logIterationInfo(int iteration, double shift) throws IOException {
-        FileWriter fw = new FileWriter("map_reduce_log.txt", true);
-        BufferedWriter bw = new BufferedWriter(fw);
-        PrintWriter out = new PrintWriter(bw);
-        out.println("MapReduce Iteration: " + iteration + ", Shift Value: " + shift + ", Converge Threshold: " + DEFAULT_THRESHOLD);
-    }
-
+	
+	public static final int DEFAULT_NUM_REDUCERS = 1;
+	public static final double DEFAULT_THRESHOLD = 0.001;
+	public static final int DEFAULT_MAX_ITERATIONS = 20;
+	
+	public static final String OUTPUT_NAME = "/output_centroids";
+	
+	
+	/**
+	 * Extract the centroids from file
+	 *
+	 * @param pathString path to the file that contains the centroids
+	 * @return ArrayList of Centroid objects
+	 * @throws IOException if an I/O error occurs during file reading
+	 */
+	public static ArrayList<Centroid> readCentroids(String pathString, Configuration conf, boolean csvReading) throws IOException {
+		ArrayList<Centroid> centroids = new ArrayList<>();
+		Path path = new Path(pathString);
+		FileSystem hdfs = FileSystem.get(conf);
+		FSDataInputStream in = hdfs.open(path);
+		BufferedReader reader = new BufferedReader(new InputStreamReader(in));
+		String line;
+		
+		while ((line = reader.readLine()) != null) {
+			
+			String[] fields = line.split("\\s");
+			
+			if (csvReading) {
+				fields = line.split(",");
+			}
+			
+			// Creare un nuovo oggetto Centroid
+			Centroid centroid = new Centroid();
+			
+			// Impostare l'ID del centroide
+			int centroidId = (int) Double.parseDouble(fields[0]);
+			centroid.getCentroid_id().set(centroidId);
+			
+			ArrayList<Double> coords = new ArrayList<>();
+			for (int i = 1; i < fields.length; i++) {
+				double value = Double.parseDouble(fields[i]);
+				coords.add(value);
+			}
+			Point point = new Point(coords);
+			centroid.setPoint(point);
+			centroids.add(centroidId, centroid);
+		}
+		
+		in.close();
+		Collections.sort(centroids);
+		return centroids;
+	}
+	
+	public static Job configureJob(Configuration conf, Path inputPath, Path outputPath, int numReducers, int iteration) {
+		Job job;
+		try {
+			job = Job.getInstance(conf, "K-Means Iteration " + iteration);
+			job.setJarByClass(KMeans.class);
+			job.setMapperClass(KMeansMapper.class);
+			job.setCombinerClass(KMeansCombiner.class);
+			job.setMapOutputKeyClass(IntWritable.class);
+			job.setMapOutputValueClass(Point.class);
+			job.setNumReduceTasks(numReducers);
+			job.setReducerClass(KMeansReducer.class);
+			job.setOutputKeyClass(IntWritable.class);
+			job.setOutputValueClass(Text.class);
+			FileInputFormat.addInputPath(job, inputPath);
+			FileOutputFormat.setOutputPath(job, outputPath);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
+		
+		return job;
+	}
+	
+	public static void mergeOutput(Configuration conf, Path outputPath, int iteration) throws IOException {
+		FileSystem fs = FileSystem.get(conf);
+		
+		Path outputFile = new Path(outputPath, OUTPUT_NAME + iteration);
+		OutputStream out = fs.create(outputFile);
+		for (FileStatus fileStatus : fs.listStatus(outputPath)) {
+			if (fileStatus.isFile() && !fileStatus.getPath().equals(outputFile)) {
+				InputStream in = fs.open(fileStatus.getPath());
+				IOUtils.copyBytes(in, out, conf);
+				in.close();
+			}
+		}
+		out.close();
+	}
+	
+	public static ArrayList<Centroid> readCentroidsFromConfiguration(Configuration conf) {
+		
+		ArrayList<Centroid> centroids = new ArrayList<>();
+		
+		String[] centroidStrings = conf.getStrings("centroids");
+		// Get the values of the centroids
+		for (int i = 0; i < centroidStrings.length; i++) {
+			centroids.add(new Centroid(i, Arrays.stream(centroidStrings[i].split(" "))
+					.map(Double::parseDouble)
+					.collect(Collectors.toCollection(ArrayList::new))));
+		}
+		return centroids;
+	}
+	
+	public static double calculateCentroidShift(ArrayList<Centroid> currentCentroids, Configuration conf) {
+		
+		// Read previous centroids
+		ArrayList<Centroid> previousCentroids = readCentroidsFromConfiguration(conf);
+		
+		// Calculate centroid shift
+		double shift = 0.0;
+		for (int i = 0; i < previousCentroids.size(); i++) {
+			ArrayList<Double> previousCentroidCoord = previousCentroids.get(i).getPoint().getCoordinates();
+			ArrayList<Double> currentCentroidCoord = currentCentroids.get(i).getPoint().getCoordinates();
+			for (int j = 0; j < previousCentroidCoord.size(); j++) {
+				shift += Math.abs(currentCentroidCoord.get(j) - previousCentroidCoord.get(j));
+			}
+		}
+		return shift;
+	}
+	
+	public static void setCentroidsToConf(String key, ArrayList<Centroid> centroids, Configuration conf) {
+		//[centroid.getPoint().toString() for centroid in centroids]
+		//Per ogni centroide nella lista di centroidi mi prendo la stringa delle coordinate del punto
+		conf.setStrings(key, centroids.stream()
+				.map(centroid -> centroid.getPoint().toString())
+				.toArray(String[]::new));
+	}
+	
+	public static void logIterationInfo(int iteration, double shift) throws IOException {
+		FileWriter fw = new FileWriter("map_reduce_log.txt", true);
+		BufferedWriter bw = new BufferedWriter(fw);
+		PrintWriter out = new PrintWriter(bw);
+		out.println("MapReduce Iteration: " + iteration + ", Shift Value: " + shift + ", Converge Threshold: " + DEFAULT_THRESHOLD);
+	}
+	
 }
