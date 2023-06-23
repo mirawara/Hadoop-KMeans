d1deb611e94e54778ea49f2d9acfbbb2be9e88ef
Fabrizio Lanzillo <fabriziolanzillo@gmail.com>
2023-06-23 15:40:21 +0200

    add of the test results and fix of the dataset gen alg

--- scripts/plotter.m
+++ scripts/plotter.m
@@ -17,7 +17,7 @@ figure
 % Traccia i dati
 plot3(data{1}(:,1), data{1}(:,2), data{1}(:,3), 'bo','Linestyle','None')
 hold on
-plot3(data{2}(:,1), data{2}(:,2), data{2}(:,3), 'g^','Linestyle','None','MarkerFaceColor', 'g')
+plot3(data{2}(:,2), data{2}(:,3), data{2}(:,4), 'g^','Linestyle','None','MarkerFaceColor', 'g')
 plot3(data{3}(:,1), data{3}(:,2), data{3}(:,3), 'rs','Linestyle','None','MarkerFaceColor', 'r')
 
 % Aggiungi etichette e titolo al plot
