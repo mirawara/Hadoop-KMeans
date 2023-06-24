7ce907db5c387c804dc68105b897f07b3e866d32
Fabrizio Lanzillo <86826030+FabrizioLanzillo@users.noreply.github.com>
2023-06-24 11:20:19 +0200

    Update hadoop_test.sh

--- scripts/hadoop_test.sh
+++ scripts/hadoop_test.sh
@@ -6,15 +6,8 @@ fi
 
 num_reducers="$1"
 
-#for i in $(seq 7 7)
-#do
-#    test="test$i"
-#    ./hadoop_run.sh $test $num_reducers
-#done
-
-
-./hadoop_run.sh test1 $num_reducers
-./hadoop_run.sh test2 $num_reducers
-./hadoop_run.sh test3 $num_reducers
-./hadoop_run.sh test4 $num_reducers
-./hadoop_run.sh test6 $num_reducers
+for i in $(seq 1 7)
+do
+    test="test$i"
+    ./hadoop_run.sh $test $num_reducers
+done
